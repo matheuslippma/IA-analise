797d42aec0f91f6535a5af976488c9c5e7868b45
matheuslippma <matheuslippma@gmail.com>
2025-06-10 23:48:38 -0300

    Create Counter.tsx

---
+++ src/components/Counter.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+const Counter: React.FC = () => {
+  const [count, setCount] = useState<number>(0);
+
+  return (
+    <div>
+      <h2>Contador: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>Incrementar</button>
+      <button onClick={() => setCount(count - 1)}>Decrementar</button>
+    </div>
+  );
+};
+
+export default Counter;
